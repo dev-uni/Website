52b9c499a182a569d0d810138d44637f45e094ac
Muasyah <dmuasyapeter@gmail.com>
2016-07-04 10:02:22 +0300

    Home page updated
    this is an update of the homepage. Your suggestions please

--- HomepageX.php
+++ HomepageX.php
@@ -12,47 +12,51 @@
 
 <div class="container">
   <header>
-    <p><h2><center>Uni dev</center></h2></p>
+    <p><h2><center>UniDev</center></h2></p>
+    <P><h3><center>Developers' meeting place</center></h3></P>
   </header>
   <div class="sidebar1">
   <nav>
     <ul>
       <li><a href="forums.html">Forums</a></li>
-      <li><a href="#">Link two</a></li>
-      <li><a href="#">Link three</a></li>
-      <li><a href="#">Link four</a></li>
+      <li><a href="contacts">Our Contacts</a></li>
+      <li><a href="mission&Vision">Mission & vision</a></li>
+      <li><a href="sourceCodes.html">Source code</a></li>
     </ul>
 </nav>
     <aside>
-      <p>Google ads here</p>
+      <p>&nbsp;</p>
     </aside>
   <!-- end .sidebar1 --></div>
   <article class="content">
-    <h1>Home</h1>
+    <h3>Home</h3>
     <section>
-     <h2>How to use this document</h2>
-      <p>Be aware that the CSS for these layouts is heavily commented. If you do most of your work in Design view, have a peek at the code to get tips on working with the CSS for the fixed layouts. You can remove these comments before you launch your site. To learn more about the techniques used in these CSS Layouts, read this article at Adobe's Developer Center - <a href="http://www.adobe.com/go/adc_css_layouts">http://www.adobe.com/go/adc_css_layouts</a>.</p>
+     <h4>Mission and Vision</h4>
+      <p><h5>Our Mission</h5></p>
+      <p>Mission statement here</p>
+      <p><h5>Our Vision</h5></p>
+      <p>Vision statement here</p>
     </section>
     <section>
-      <h2>Clearing Method</h2>
-      <p>Because all the columns are floated, this layout uses a clear:both declaration in the footer rule.  This clearing technique forces the .container to understand where the columns end in order to show any borders or background colors you place on the .container. If your design requires you to remove the footer from the .container, you'll need to use a different clearing method. The most reliable will be to add a &lt;br class=&quot;clearfloat&quot; /&gt; or &lt;div  class=&quot;clearfloat&quot;&gt;&lt;/div&gt; after your final floated column (but before the .container closes). This will have the same clearing effect. </p>
+      <h4>Forums</h4>
+      <p>Description of our forums here</p>
     </section>
+    
     <section>
-      <h2>Logo Replacement</h2>
-      <p>An image placeholder was used in this layout in the header where you'll likely want to place  a logo. It is recommended that you remove the placeholder and replace it with your own linked logo. </p>
-      <p> Be aware that if you use the Property inspector to navigate to your logo image using the SRC field (instead of removing and replacing the placeholder), you should remove the inline background and display properties. These inline styles are only used to make the logo placeholder show up in browsers for demonstration purposes. </p>
-      <p>To remove the inline styles, make sure your CSS Styles panel is set to Current. Select the image, and in the Properties pane of the CSS Styles panel, right click and delete the display and background properties. (Of course, you can always go directly into the code and delete the inline styles from the image or placeholder there.)</p>
+      <h4>Source code</h4>
+      <p>Description of source code section</p>
+      <p>more sections to be added</p>
     </section>
   <!-- end .content --></article>
   <aside>
-    <h4>Important Links</h4>
     <p><a href="login.html">Log in</a></p>
     <p><a href="signup.html">Sign up</a></p>
     <p><a href="developers.html">Our developers</a></p>
-    <p>Advert here</p>
+    <p><a href="upload.html">Upload code</a></p>
   </aside>
   <footer>
-    <p><center><a href="aboutUs.html">About us</a> | <a href="sitemap.html">Sitemap</a> | &copy;Unidev 2016</center></p>
+    <p><center>
+    <img src="disc burner.jpg" width="83" height="25"  alt=""/> | <a href="aboutUs.html">About us</a> | <a href="sitemap.html">Sitemap</a> | &copy;Unidev 2016</center></p>
   </footer>
   <!-- end .container --></div>
 </body>
